5300924650f0e6a5c694041a866f1d3e6903464a
Jan Goldmann <Jan.Goldmann@clear-it.de>
2019-04-29 15:17:33 +0200

    Regeln (Tests) gefixed und erweitert

--- src/main/java/de/clearit/games/gameoflife/GameOfLife.java
+++ src/main/java/de/clearit/games/gameoflife/GameOfLife.java
@@ -8,6 +8,8 @@ public class GameOfLife {
 
     public void init(int x, int y) {
         cellBoard = new Cell[x][y];
+
+        // In jedes Feld/ in jeden Index eine tote Zelle setzen
     }
 
 
@@ -17,22 +19,22 @@ public class GameOfLife {
         // suche in Array
 
         // Zellen oben
-        for (int i = cellPosition.x - 1; i < 3; i++) {
-            if (hasPositionLivingCell(new Point(cellPosition.y - 1, i))) {
+        for (int i = cellPosition.x - 1; i < ((cellPosition.x - 1) + 3); i++) {
+            if (hasPositionLivingCell(new Point(i, cellPosition.y - 1))) {
                 count++;
             }
         }
         // Zellen unten
-        for (int i = cellPosition.x - 1; i < 3; i++) {
-            if (hasPositionLivingCell(new Point(cellPosition.y + 1, i))) {
+        for (int i = cellPosition.x - 1; i < ((cellPosition.x - 1) + 3); i++) {
+            if (hasPositionLivingCell(new Point(i, cellPosition.y + 1))) {
                 count++;
             }
         }
         // Zellen links und rechts
-        if (hasPositionLivingCell(new Point(cellPosition.y, cellPosition.x - 1))) {
+        if (hasPositionLivingCell(new Point(cellPosition.x - 1, cellPosition.y))) {
             count++;
         }
-        if (hasPositionLivingCell(new Point(cellPosition.y, cellPosition.x + 1))) {
+        if (hasPositionLivingCell(new Point(cellPosition.x + 1, cellPosition.y))) {
             count++;
         }
 
@@ -41,7 +43,7 @@ public class GameOfLife {
 
     private boolean hasPositionLivingCell(Point cellPosition) {
         try {
-            Cell checkCell = cellBoard[cellPosition.y][cellPosition.x];
+            Cell checkCell = cellBoard[cellPosition.x][cellPosition.y];
             if (checkCell != null && checkCell.isAlive()) {
                 return true;
             }
@@ -56,6 +58,8 @@ public class GameOfLife {
     public boolean oracleDecision(int livingNeighborsCount) {
         if (livingNeighborsCount < 2 || livingNeighborsCount > 3) {
             return false;
+        } else if (livingNeighborsCount == 3) {
+            return true;
         } else {
             return true;
         }
@@ -66,18 +70,22 @@ public class GameOfLife {
     }
 
     public void setCellAt(Point point, Cell cell) {
-        cellBoard[point.y][point.x] = cell;
+        cellBoard[point.x][point.y] = cell;
     }
 
     public void markOfDeath() {
         for (int row = 0; row < cellBoard.length; row++) {
             for (int column = 0; column < cellBoard[row].length; column++) {
                 // Überprüfen ob Zelle vorhanden
-                if (cellBoard[row][column] != null) {
+                if (cellBoard[column][row] != null) {
                     // Anzahl der lebenden Nachbarn ermitteln
-                    int livingNeighbors = getCountOfLivingNeighborCells(new Point(row, column));
+                    int livingNeighbors = getCountOfLivingNeighborCells(new Point(column, row));
                     // Oracle setzen
-                    cellBoard[row][column].setOracle(oracleDecision(livingNeighbors));
+                    if (livingNeighbors != 2) {
+                        cellBoard[column][row].setOracle(oracleDecision(livingNeighbors));
+                    } else {
+                        cellBoard[column][row].setOracle(cellBoard[column][row].isAlive());
+                    }
                 }
 
             }
@@ -88,9 +96,9 @@ public class GameOfLife {
         for (int row = 0; row < cellBoard.length; row++) {
             for (int column = 0; column < cellBoard[row].length; column++) {
                 // Überprüfen ob Zelle vorhanden
-                if (cellBoard[row][column] != null) {
+                if (cellBoard[column][row] != null) {
                     // Natural selektieren
-                    cellBoard[row][column].setAlive(cellBoard[row][column].getOracle());
+                    cellBoard[column][row].setAlive(cellBoard[column][row].getOracle());
                 }
 
             }
--- src/test/java/de/clearit/games/gameoflife/GameOfLifeTests.java
+++ src/test/java/de/clearit/games/gameoflife/GameOfLifeTests.java
@@ -10,8 +10,8 @@ import java.awt.*;
 public class GameOfLifeTests {
 
     @Test
-    @DisplayName("Spielregel 1 Tests")
-    public void spielregel1Test(){
+    @DisplayName("Spielregel 1, Test: Positiv")
+    public void spielregel1TestPositiv() {
         // Eine tote Zelle mit genau drei lebenden Nachbarn
         // wird in der Folgegeneration neu geboren.
 
@@ -23,17 +23,10 @@ public class GameOfLifeTests {
         Cell cell2 = new CellImpl();
         cell2.setAlive(false);
 
-        gameOfLife.setCellAt(new Point(0,0), cell);
-        gameOfLife.setCellAt(new Point(0,1), new CellImpl());
-        gameOfLife.setCellAt(new Point(1,0), new CellImpl());
-        gameOfLife.setCellAt(new Point(1,1), new CellImpl());
-
-//        System.out.println(cell.isAlive());
-//        System.out.println(gameOfLife.getSalAd(0,1).isAlive());
-//        System.out.println(cell2.isAlive());
-//
-//        System.out.println(gameOfLife.getSalAd(1,0).isAlive());
-//        System.out.println(gameOfLife.getSalAd(1,1).isAlive());
+        gameOfLife.setCellAt(new Point(0, 0), cell);
+        gameOfLife.setCellAt(new Point(0, 1), new CellImpl());
+        gameOfLife.setCellAt(new Point(1, 0), new CellImpl());
+        gameOfLife.setCellAt(new Point(1, 1), new CellImpl());
 
         gameOfLife.markOfDeath();
 
@@ -44,15 +37,14 @@ public class GameOfLifeTests {
         Assertions.assertTrue(cell.isAlive());
 
 
-
     }
 
     @Test
-    @DisplayName("Spielregel 1 Tests 2")
-    public void spielregel1TestFehler(){
-        // Eine tote Zelle mit genau zwei lebenden und einem toten Nachbarn
-        // wird in der Folgegeneration NICHT neu geboren.
-        // Lebende Zellen sterben wegen Einsamkeit.
+    @DisplayName("Spielregel 1, Test: Negativ")
+    public void spielregel1TestNegativ() {
+        /* Eine tote Zelle mit genau zwei lebenden und einem toten Nachbarn
+           wird in der Folgegeneration NICHT neu geboren.
+           Lebende Zellen sterben wegen Einsamkeit. */
 
         GameOfLife gameOfLife = new GameOfLife();
         gameOfLife.init(100, 100);
@@ -62,37 +54,100 @@ public class GameOfLifeTests {
         Cell cell2 = new CellImpl();
         cell2.setAlive(false);
 
-        gameOfLife.setCellAt(new Point(0,0), cell);
-        gameOfLife.setCellAt(new Point(0,1), new CellImpl());
-        gameOfLife.setCellAt(new Point(1,0), new CellImpl());
-        gameOfLife.setCellAt(new Point(1,1), new CellImpl());
+        gameOfLife.setCellAt(new Point(0, 0), cell);
+        gameOfLife.setCellAt(new Point(0, 1), cell2);
+        gameOfLife.setCellAt(new Point(1, 0), new CellImpl());
+        gameOfLife.setCellAt(new Point(1, 1), new CellImpl());
 
-        System.out.println(cell.isAlive());
-        System.out.println(gameOfLife.getSalAd(0,1).isAlive());
-        System.out.println(cell2.isAlive());
+        // Zellen vor der Selektion
+        System.out.print("Before:\t\t\t\t\t");
+        System.out.print(cell.isAlive() + " ");                         //f
+        System.out.print(gameOfLife.getSalAd(0, 0).isAlive() + " ");    //f
+        System.out.print(cell2.isAlive() + " ");                        //f
+        System.out.print(gameOfLife.getSalAd(0, 1).isAlive() + " ");    //f
+        System.out.print(gameOfLife.getSalAd(1, 0).isAlive() + " ");    //t
+        System.out.println(gameOfLife.getSalAd(1, 1).isAlive());        //t
 
-        System.out.println(gameOfLife.getSalAd(1,0).isAlive());
-        System.out.println(gameOfLife.getSalAd(1,1).isAlive());
-
+        // Todesmarke setzen
         gameOfLife.markOfDeath();
+
+        System.out.print("Mark of death:\t\t\t");
+        System.out.print(cell.getOracle() + " ");                       //f
+        System.out.print(gameOfLife.getSalAd(0, 0).getOracle() + " ");  //f
+        System.out.print(cell2.getOracle() + " ");                      //f
+        System.out.print(gameOfLife.getSalAd(0, 1).getOracle() + " ");  //f
+        System.out.print(gameOfLife.getSalAd(1, 0).getOracle() + " ");  //f
+        System.out.println(gameOfLife.getSalAd(1, 1).getOracle());      //f
+
 
         // Neue Generation
         gameOfLife.naturalSelection();
 
-
-        System.out.println(cell.isAlive());
-        System.out.println(gameOfLife.getSalAd(0,1).isAlive());
-        System.out.println(cell2.isAlive());
-
-        System.out.println(gameOfLife.getSalAd(1,0).isAlive());
-        System.out.println(gameOfLife.getSalAd(1,1).isAlive());
+        System.out.print("Natural Selection:\t\t" );
+        System.out.print(cell.isAlive() + " ");                         //f
+        System.out.print(gameOfLife.getSalAd(0, 0).isAlive() + " ");    //f
+        System.out.print(cell2.isAlive() + " ");                        //f
+        System.out.print(gameOfLife.getSalAd(0, 1).isAlive() + " ");    //f
+        System.out.print(gameOfLife.getSalAd(1, 0).isAlive() + " ");    //f
+        System.out.print(gameOfLife.getSalAd(1, 1).isAlive() + " ");    //f
 
 
         // Erwartung: Zelle neu geboren
         Assertions.assertFalse(cell.isAlive());
 
 
+    }
+
+    @Test
+    @DisplayName("Spielregel 2, Test: Positiv")
+    public void spielregel2TestPositiv() {
+
+        GameOfLife gameOfLife = new GameOfLife();
+        gameOfLife.init(100, 100);
+
+        Cell cell = new CellImpl();
+        Cell cell2 = new CellImpl();
+
+        gameOfLife.setCellAt(new Point(0, 0), cell);
+        gameOfLife.setCellAt(new Point(0, 1), cell2);
+
+        gameOfLife.markOfDeath();
+
+        gameOfLife.naturalSelection();
+
+        System.out.print(cell.isAlive() + " ");    //f
+        System.out.print(cell2.isAlive() + " ");    //f
+
+        Assertions.assertFalse(cell.isAlive());
+
 
     }
 
+    @Test
+    @DisplayName("Spielregel 3, Test: Positiv")
+    public void spielregel3TestPositiv() {
+
+        GameOfLife gameOfLife = new GameOfLife();
+        gameOfLife.init(100, 100);
+
+        Cell cell = new CellImpl();
+        Cell cell2 = new CellImpl();
+        Cell cell3 = new CellImpl();
+        Cell cell4 = new CellImpl();
+
+        gameOfLife.setCellAt(new Point(0, 0), cell);
+        gameOfLife.setCellAt(new Point(0, 1), cell2);
+         gameOfLife.setCellAt(new Point(1, 0), cell3);
+
+        gameOfLife.markOfDeath();
+
+        gameOfLife.naturalSelection();
+
+        System.out.print(cell.isAlive() + " ");
+        System.out.print(cell2.isAlive() + " ");
+        System.out.print(cell3.isAlive() + " ");
+        System.out.print(cell4.isAlive() + " ");
+
+        Assertions.assertTrue(cell.isAlive());
+    }
 }
